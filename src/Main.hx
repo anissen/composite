42062d1c4393785c5e5b9705f2a7bbd838807827
Anders Nissen <andnis@gmail.com>
2022-06-24 21:42:37 +0200

    checkpoint

--- src/Main.hx
+++ src/Main.hx
@@ -1,3 +1,4 @@
+import haxe.display.Display.DeterminePackageResult;
 
 // Based on https://ajmmertens.medium.com/building-an-ecs-1-types-hierarchies-and-prefabs-9f07666a1e9d
 abstract EntityId(haxe.Int32) from Int to Int {}
@@ -9,14 +10,14 @@ typedef Components = Array<EntityId>;
 final InstanceOf: EntityId = 1 << 29;
 final ChildOf: EntityId = 2 << 28;
 
-// @:structInit
-// class EcsComponent {
-// }
+@:structInit
+class EcsComponent {
+}
 
-// @:structInit
-// class EcsId {
-// 	public final name: String;
-// }
+@:structInit
+class EcsId {
+	public final name: String;
+}
 
 // @:structInit
 // class Position {
@@ -32,6 +33,34 @@ final ChildOf: EntityId = 2 << 28;
 // @:structInit
 // class Player {}
 
+@:structInit
+class Edge {
+	public var add: Null<Archetype>;
+	public var remove: Null<Archetype>;
+}
+
+@:structInit
+class Archetype {
+	public final type: Components;
+	public final entityIds: Array<EntityId>;
+	public final components: Array<Array<Any>>;
+	// public final length: Int;
+	// public final edges: Array<Edge>;
+	public final edges: Map<EntityId, Edge>;
+	public function toString() {
+		return 'Archetype { type: $type, entityId: $entityIds, edges: $edges }';
+	}
+}
+
+@:structInit
+class Record {
+	public final archetype: Archetype;
+	public final row: Int;
+	public function toString() {
+		return 'Record { archetype: $archetype, row: $row }';
+	}
+}
+
 // TODO: Make enum
 final EcsComponent_id = 1;
 final EcsId_id = 2;
@@ -45,11 +74,38 @@ class Main {
 		final c = new Main();
 	}
 
-	final entityIndex = new Map<EntityId, Components>();
+	final entityIndex = new Map<EntityId, Record>();
+	final emptyArchetype: Archetype = {
+		type: [],
+		entityIds: [],
+		components: [],
+		// length: 0,
+		edges: [],
+	};
 
 	inline public function new() {
-		entityIndex.set(EcsComponent_id, [EcsComponent_id, EcsId_id]);
-		entityIndex.set(EcsId_id, [EcsComponent_id, EcsId_id]);
+		final destinationArchetype = findOrCreateArchetype(emptyArchetype, [EcsComponent_id, EcsId_id]);
+		// trace(destinationArchetype);
+		// trace(destinationArchetype.entityIds);
+		// trace(destinationArchetype.components);
+		destinationArchetype.entityIds.push(EcsComponent_id);
+		// trace(destinationArchetype.components);
+		destinationArchetype.components[0].push(({}: EcsComponent));
+		destinationArchetype.components[1].push(({ name: 'EcsComponent' }: EcsId));
+		final componentRecord: Record = {
+			archetype: destinationArchetype,
+			row: destinationArchetype.entityIds.length - 1,
+		};	
+		entityIndex.set(EcsComponent_id, componentRecord);
+		
+		destinationArchetype.entityIds.push(EcsId_id);
+		destinationArchetype.components[0].push(({}: EcsComponent));
+		destinationArchetype.components[1].push(({ name: 'EcsId' }: EcsId));
+		final idRecord: Record = {
+			archetype: destinationArchetype,
+			row: destinationArchetype.entityIds.length - 1,
+		};	
+		entityIndex.set(EcsId_id, idRecord);
 		testEcs();	
 	}
 
@@ -57,33 +113,155 @@ class Main {
 		addEntity(Player_id);
 		addComponent(Player_id, Health_id);
 		addComponent(Player_id, Position_id);
-		// trace((2: haxe.Int64) | (8: haxe.Int64));
-		final x = (ChildOf | Faction_id);
-		trace(x);
-		addComponent(Player_id, x);
+		// final x = (ChildOf | Faction_id);
+		// trace(x);
+		// addComponent(Player_id, x);
 
-		trace('player is child of faction?');
-		trace((x & ChildOf > 0) ? 'yes' : 'no');
+		// trace('player is child of faction?');
+		// trace((x & ChildOf > 0) ? 'yes' : 'no');
 
 		// trace(entityIndex);
+		trace('entities and components in entityIndex:');
 		for (entity => components in entityIndex) {
 			trace('$entity: $components');
 		}
 	}
 
 	inline function addEntity(entity: EntityId) {
-		entityIndex.set(entity, [EcsId_id]);
+		final destinationArchetype = findOrCreateArchetype(emptyArchetype, [EcsId_id]);
+		// destinationArchetype.components.push({})
+		destinationArchetype.entityIds.push(entity);
+		final record: Record = {
+			archetype: destinationArchetype,
+			row: destinationArchetype.entityIds.length - 1,
+		};
+		entityIndex.set(entity, record);
 	}
 	
 	inline function addComponent(entity: EntityId, componentId: EntityId) {
 		if (!entityIndex.exists(entity)) throw 'entity $entity does not exist';
-		if (!entityIndex.exists(componentId)) {
-			entityIndex.set(componentId, [EcsComponent_id, EcsId_id]);
+		// if (!entityIndex.exists(componentId)) {
+		// 	entityIndex.set(componentId, [EcsComponent_id, EcsId_id]);
+		// }
+		final archetype = entityIndex[entity].archetype;
+		final type = archetype.type;
+		if (type.contains(componentId)) {
+			trace('component $componentId already exists on entity $entity');
+			return;
 		}
-		entityIndex[entity].push(componentId);
+		
+		// find destination archetype
+		var node = archetype;
+		for (t in archetype.type) {
+			trace(t);
+			var edge = node.edges[t];
+			trace(edge);
+			if (edge == null) { // TODO: this is wrong? or a hack at least
+				edge = {
+					add: null,
+					remove: null,
+				};
+			}
+			if (edge.add == null) {
+				edge.add = createArchetype(node, t);
+			}
+			// if (edge.add.type.contains(componentId)) {
+			// 	node = edge.add;
+			// 	break;
+			// }
+			node = edge.add;
+		}
+		
+		// insert entity into component array of destination
+		// TODO: Implement!
+		
+		// copy overlapping components from source to destination
+		// TODO: Implement!
+		
+		// remove entity from component array of source
+		// TODO: Implement!
 	}
 
-	inline function hasComponent(entity: EntityId, component: EntityId) {
-		return entityIndex[entity].contains(component);
+	inline function findOrCreateArchetype(archetype: Archetype, type: Components): Archetype {
+		// find destination archetype
+		var node = archetype;
+		for (t in type) {
+			trace(t);
+			var edge = node.edges[t];
+			trace(edge);
+			if (edge == null) { // TODO: this is wrong? or a hack at least
+				edge = {
+					add: null,
+					remove: null,
+				};
+			}
+			if (edge.add == null) {
+				edge.add = createArchetype(node, t);
+			}
+			// if (edge.add.type.contains(componentId)) {
+			// 	node = edge.add;
+			// 	break;
+			// }
+			node = edge.add;
+		}
+		return node;
+	}
+
+	inline function createNewArchetype(componentIds: Components) {
+		return {
+			type: componentIds,
+			entityIds: [],
+			components: [],
+			length: 0,
+			edges: [], // TODO: create edges
+		};
+	}
+
+	inline function createArchetype(archetype: Archetype, addComponentId: EntityId): Archetype {
+		trace('createArchetype');
+		final type = archetype.type.concat([addComponentId]);
+		return {
+			type: type,
+			entityIds: [],
+			components: [for (_ in type) []],
+			edges: [
+				addComponentId => {
+					add: null,
+					remove: archetype
+				}
+			], // TODO: create correct edges
+		};
+		// final type = archetype.type;
+		// final entityIds = archetype.entityIds;
+		// final components = archetype.components;
+		// final length = archetype.length;
+		// final edges = archetype.edges;
+		// final newArchetype = new Archetype();
+		// newArchetype.type = type.concat(componentId);
+		// newArchetype.entityIds = entityIds;
+		// newArchetype.components = components;
+		// newArchetype.length = length;
+		// newArchetype.edges = edges;
+		// return newArchetype;
+	}
+
+	inline function getComponent(entity: EntityId, componentId: EntityId): Any {
+		final record = entityIndex[entity];
+		final archetype = record.archetype;
+		final type = archetype.type;
+		for (t in type) {
+			if (t == componentId) return archetype.components[record.row];
+		}
+		return null;
+	}
+
+	inline function hasComponent(entity: EntityId, componentId: EntityId) {
+		final record = entityIndex[entity];
+		final archetype = record.archetype;
+		final type = archetype.type;
+		for (t in type) {
+			if (t == componentId) return true;
+		}
+		return false;
 	}
 }
