6a287885d48cf1a34bd8162b59eaf70fb8ae7526
Anders Nissen <andnis@gmail.com>
2022-07-13 09:31:18 +0200

    Canvas improvements

--- examples/Canvas.hx
+++ examples/Canvas.hx
@@ -5,10 +5,28 @@ import js.Browser;
 import js.Browser.document;
 import js.html.CanvasRenderingContext2D;
 
+@:structInit
+final class Position implements Composite.Component {
+	public var x: Float;
+	public var y: Float;
+	public function toString() {
+		return 'Position { x: $x, y: $y }';
+	}
+}
+
+@:structInit
+final class Velocity implements Composite.Component {
+	public var x: Float;
+	public var y: Float;
+	public function toString() {
+		return 'Velocity { x: $x, y: $y }';
+	}
+}
+
 inline function main() {
     final canvas = document.createCanvasElement();
-    canvas.width = 400;
-    canvas.height = 400;
+    canvas.width = 800;
+    canvas.height = 800;
     document.body.appendChild(canvas);
 
     init();
@@ -29,7 +47,8 @@ inline function main() {
 
     canvas.onclick = (event -> {
         final e = context.createEntity('Entity');
-        context.addComponent(e, ({ x: -1 + 2 * Math.random(), y: -1 + 2 * Math.random() }: Velocity));
+        final speed = 2.0;
+        context.addComponent(e, ({ x: -speed + 2 * speed * Math.random(), y: -speed + 2 * speed * Math.random() }: Velocity));
         context.addComponent(e, ({ x: event.clientX, y: event.clientY }: Position));
     });
 
@@ -39,42 +58,30 @@ inline function main() {
 		for (i in 0...position.length) {
 			position[i].x += velocity[i].x;
 			position[i].y += velocity[i].y;
+            if (position[i].x < 0 || position[i].x > canvas.width) {
+                velocity[i].x = -velocity[i].x;
+            }
+            if (position[i].y < 0 || position[i].y > canvas.height) {
+                velocity[i].y = -velocity[i].y;
+            }
 		}
 	}, 'MoveSystem');
     
     context.addSystem(Include(Position.ID), (components) -> {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        // ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.fillStyle = 'rgb(75, 220, 255)';
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     
-        ctx.fillStyle = '#ff0000';
-        ctx.strokeStyle = 'rgba(0, 153, 255)';
+        ctx.fillStyle = 'rgb(0, 100, 255)';
 
 		final position: Array<Position> = components[0];
         final radius = 5.0;
 		for (pos in position) {
             ctx.beginPath();
             ctx.arc(pos.x, pos.y, radius, 0, Math.PI * 2);
-            ctx.stroke();
+            ctx.fill();
 		}
 	}, 'RenderSystem');
-}
-
-@:structInit
-final class Position implements Composite.Component {
-	public var x: Float;
-	public var y: Float;
-	public function toString() {
-		return 'Position { x: $x, y: $y }';
-	}
-}
-
-@:structInit
-final class Velocity implements Composite.Component {
-	public var x: Float;
-	public var y: Float;
-	public function toString() {
-		return 'Velocity { x: $x, y: $y }';
-	}
 }
 
 inline function init() {
