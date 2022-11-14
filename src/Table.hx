19a21589d33b75f41c2b9107d40d75ca92e19e63
Anders Nissen <andnis@gmail.com>
2022-11-14 12:52:18 +0100

    More table

--- src/Table.hx
+++ src/Table.hx
@@ -5,72 +5,77 @@ import haxe.Int32;
 import haxe.exceptions.NotImplementedException;
 
 class Table {
-	/*
-		TODO:
-		Handle table of colums. Each column has the same number of rows. The first column is the entity id, the rest are the components.
+    /*
+        TODO:
+        Handle table of colums. Each column has the same number of rows. The first column is the entity id, the rest are the components.
 
-		Must support the following actions:
-		- Add a new row (could be reusing an deleted row)
-		- Delete a row (i.e. mark it as deleted by moving it to the end)
-		- Get the live number of rows
-		- Move a row from one table to another (potentially with more or fewer columns)
-	 */
-	var ids:Array<EntityId>;
-	var columns:Array<Array<Any>>;
+        Must support the following actions:
+        - Add a new row (could be reusing an deleted row)
+        - Delete a row (i.e. mark it as deleted by moving it to the end)
+        - Get the live number of rows
+        - Move a row from one table to another (potentially with more or fewer columns)
+     */
+    var ids: Array<EntityId>;
+    var columns: Array<Array<Any>>;
 
-	public function new(numberOfColumns:Int) {
-		ids = [];
-		columns = [for (i in 0...numberOfColumns) []];
-	}
+    public function new(numberOfColumns: Int) {
+        ids = [];
+        columns = [for (i in 0...numberOfColumns) []];
+    }
 
-	public function addRow(id:EntityId, data:Array<Any>) {
-		if (data.length != columns.length)
-			throw 'data length does not match column count';
+    public function addRow(id: EntityId, data: Array<Any>) {
+        if (data.length != columns.length)
+            throw 'data length does not match column count';
 
-		ids.push(id);
-		for (i => d in data) {
-			columns[i].push(d);
-		}
-	}
+        ids.push(id);
+        for (i => d in data) {
+            columns[i].push(d);
+        }
+    }
 
-	public function deleteRow(row:Int) {
-		if (row < 0 || row >= ids.length)
-			throw 'row $row is out of bounds';
+    public function deleteRow(row: Int) {
+        if (row < 0 || row >= ids.length)
+            throw 'row $row is out of bounds';
 
-		// TODO: Implement a better row deletion (swap and mark dead?)
-		ids.splice(row, 1);
-		for (c in columns) {
-			c.splice(row, 1);
-		}
-	}
+        // TODO: Implement a better row deletion (swap and mark dead?)
+        ids.splice(row, 1);
+        for (c in columns) {
+            c.splice(row, 1);
+        }
+    }
 
-	public function getRow(row:Int):Array<Any> {
-		return [for (c in columns) c[row]];
-	}
+    public function getColumnCount(): Int {
+        return columns.length;
+    }
 
-	public function getRowCount():Int {
-		return ids.length;
-	}
+    public function getRow(row: Int): Array<Any> {
+        return [for (c in columns) c[row]];
+    }
 
-	public function getLiveRowCount() {
-		throw new NotImplementedException();
-	}
+    public function getRowCount(): Int {
+        return ids.length;
+    }
 
-	public function moveRow(row:Int, toTable:Table) {
-		if (row < 0 || row >= ids.length)
-			throw 'row $row is out of bounds';
+    public function getLiveRowCount() {
+        throw new NotImplementedException();
+    }
 
-		final id = ids[row];
-		toTable.addRow(id, getRow(row));
-		deleteRow(row);
-	}
+    public function moveRow(row: Int, dest: Table) {
+        if (row < 0 || row >= ids.length)
+            throw 'row $row is out of bounds';
+        if (dest.getColumnCount() != getColumnCount()) throw 'destination table column count does not match source table column count';
 
-	public function print() {
-		trace('| id | ${[for (i => c in columns) "data" + (i + 1)].join(" | ")} |');
-		trace('-----------------------');
-		for (i => id in ids) {
-			final rowStr = [for (c in columns) c[i]].join(' | ');
-			trace('| $id | $rowStr |');
-		}
-	}
+        final id = ids[row];
+        dest.addRow(id, getRow(row));
+        deleteRow(row);
+    }
+
+    public function print() {
+        trace('| id | ${[for (i => c in columns) "data" + (i + 1)].join(" | ")} |');
+        trace('-----------------------');
+        for (i => id in ids) {
+            final rowStr = [for (c in columns) c[i]].join(' | ');
+            trace('| $id | $rowStr |');
+        }
+    }
 }
--- tests/TestAll.hx
+++ tests/TestAll.hx
@@ -3,7 +3,7 @@ package tests;
 using utest.Assert;
 
 class TestAll {
-	public static function main() {
-		utest.UTest.run([new ContextTest(), new TableTest()]);
-	}
+    public static function main() {
+        utest.UTest.run([new ContextTest(), new TableTest()]);
+    }
 }
